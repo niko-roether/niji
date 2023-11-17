74ffbd4584842155a6ca64740fe345fcf87ae5c7
nicholas-roether <nicholas-roether@t-online.de>
2023-11-17 11:51:41 +0100

    Remove redundant find_*_file api

--- niji/src/lua/api/filesystem.rs
+++ niji/src/lua/api/filesystem.rs
@@ -9,27 +9,6 @@ use super::Module;
 pub struct FilesystemApi;
 
 impl FilesystemApi {
-	fn find_config_file(lua: &Lua, (path, mode): (String, String)) -> mlua::Result<mlua::Value> {
-		let xdg = lua.app_data_ref::<XdgDirs>().unwrap();
-		let joined = xdg.config_home.join(path);
-
-		Self::io_open(lua, joined, mode)
-	}
-
-	fn find_data_file(lua: &Lua, (path, mode): (String, String)) -> mlua::Result<mlua::Value> {
-		let xdg = lua.app_data_ref::<XdgDirs>().unwrap();
-		let joined = xdg.data_home.join(path);
-
-		Self::io_open(lua, joined, mode)
-	}
-
-	fn find_state_file(lua: &Lua, (path, mode): (String, String)) -> mlua::Result<mlua::Value> {
-		let xdg = lua.app_data_ref::<XdgDirs>().unwrap();
-		let joined = xdg.state_home.join(path);
-
-		Self::io_open(lua, joined, mode)
-	}
-
 	fn open_managed(lua: &Lua, path: String) -> mlua::Result<mlua::Value> {
 		let mut file_mgr = lua.app_data_mut::<FileManager>().unwrap();
 		let path = PathBuf::from(path);
@@ -53,15 +32,6 @@ impl Module for FilesystemApi {
 	fn build(lua: &Lua) -> mlua::Result<mlua::Value> {
 		let module = lua.create_table()?;
 
-		module.raw_set(
-			"find_config_file",
-			lua.create_function(Self::find_config_file)?
-		)?;
-		module.raw_set("find_data_file", lua.create_function(Self::find_data_file)?)?;
-		module.raw_set(
-			"find_state_file",
-			lua.create_function(Self::find_state_file)?
-		)?;
 		module.raw_set("open_managed", lua.create_function(Self::open_managed)?)?;
 
 		module.into_lua(lua)
