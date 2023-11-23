cf73494f944b416ccd8346f49811d8ee55016ac1
Nicholas Roether <nicholas.roether@t-online.de>
2023-11-23 19:12:16 +0100

    Fix typo

--- modules/hyprland/module.lua
+++ modules/hyprland/module.lua
@@ -3,7 +3,7 @@ local M = {}
 local template = niji.template.load("theme.conf.mustache")
 template:set_format("color", "rgba({rx}{gx}{bx}{ax})")
 
-function A.apply(config, theme)
+function M.apply(config, theme)
 	local configure_cursor = config.cursor ~= nil and config.cursor_size ~= nil
 	if not configure_cursor then
 		niji.console.debug("No cursor theme set, skipping cursor config")
@@ -13,7 +13,7 @@ function A.apply(config, theme)
 		configure_cursor = configure_cursor,
 		cursor_theme = config.cursor,
 		cursor_size = config.cursor_size,
-		border_color = theme.ui.border,
+		border_color = theme.ui.background,
 		active_border_color = theme.ui.surface,
 		shadow_color = theme.ui.shadow
 	}
@@ -24,4 +24,4 @@ function A.apply(config, theme)
 	os.execute("hyprctl reload")
 end
 
-return A;
+return M;
