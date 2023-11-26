0c44a1f4cab715c3971677bb53ed3577ec203c75
Nicholas Roether <nicholas.roether@t-online.de>
2023-11-26 23:51:34 +0100

    Implement gtk reloading

--- modules/gtk/module.lua
+++ modules/gtk/module.lua
@@ -6,4 +6,9 @@ function M.apply(config, theme)
 	install.install(config, theme)
 end
 
+function M.reload()
+	os.execute("gsettings set org.gnome.desktop.interface gtk-theme \"\"")
+	os.execute("gsettings set org.gnome.desktop.interface gtk-theme \"niji\"")
+end
+
 return M;
