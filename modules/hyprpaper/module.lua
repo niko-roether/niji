4012cbebbdac65b0284a2a5087d3e8e9fedbb395
Nicholas Roether <nicholas.roether@t-online.de>
2023-11-27 23:22:07 +0100

    Add hyprpaper module

---
+++ modules/hyprpaper/module.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local template = niji.template.load("hyprpaper.conf.mustache")
+
+function M.apply(config, theme)
+	local hyprpaper_conf = template:render {
+		wallpaper = niji.util.by_theme(theme, config.wallpaper),
+		splash = config.splash,
+		splash_offset = config.spash_offset
+	}
+
+	niji.fs.write_config("hypr/hyprpaper.conf", hyprpaper_conf)
+end
+
+function M.reload()
+	os.execute("pkill hyprpaper")
+	niji.os.exec_detached("hyprpaper > /dev/null")
+end
+
+return M
