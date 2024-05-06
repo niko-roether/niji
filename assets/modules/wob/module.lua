3400b5d3534c1d548c74e179142f391775358a3c
nicholas-roether <nicholas-roether@t-online.de>
2024-05-06 10:20:45 +0200

    Add wob module

---
+++ assets/modules/wob/module.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+local wob_ini = niji.Template:load("wob.ini.mustache")
+
+function M.apply(config, theme)
+	local custom_config = config.custom_config_file and niji.fs.read_config_asset(config.custom_config_file)
+
+	local config = wob_ini:render {
+		background_color = theme.ui.background,
+		border_color = theme.ui.border,
+		bar_color = theme.ui.primary,
+		custom_config = custom_config
+	}
+
+	niji.fs.write_config("wob/wob.ini", config)
+end
+
+function M.reload(config)
+	local wob_command = config.wob_command or "tail -f " .. niji.xdg.runtime_dir .. "/wob.sock | wob"
+	os.execute("killall wob")
+	niji.os.exec_detached(wob_command)
+end
+
+return M
